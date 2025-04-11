d626c972c99a6339aad59fda0b29f5f96787f09a
Windy Player <windyplayer@gmx.com>
2025-04-11 05:57:02 -0700

    Build and Run JAR with all dependencies included

---
+++ src/main/java/net/windyweather/panimagetwo/JarMain.java
@@ -0,0 +1,10 @@
+package net.windyweather.panimagetwo;
+
+import javafx.application.Application;
+
+public class JarMain {
+    public static void main(String[] args ) {
+        System.out.println("JarMain - app starting in main again");
+        Application.launch(PanImageTwo.class, args);
+    }
+}
